28939235b30038d1c337670fc1122bcd2e2ad91e
chanasitST <chanasit.s@stelligence.com>
2025-12-19 15:40:59 +0700

    agent04 explainer broiler plate

---
+++ agent04/explainer.py
@@ -0,0 +1,6 @@
+from typing import List
+from langchain_core.documents import Document
+from langchain_core.retrievers import BaseRetriever
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import ChatPromptTemplate
+
---
+++ agent04/main.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+from agent02.loaders import DocumentLoader
+from agent03.chunking.chunker import chunk_documents
+from agent03.chunking.retriever import create_hybrid_retriever
+from agent04.explainer import generate_explanation
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+load_dotenv()
